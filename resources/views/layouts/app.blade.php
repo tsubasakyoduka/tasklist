d4a8be7b3c3833ad6c24ddb7a1f6b4029c6f4813
tsubasa kyouduka <tubasa.0312@outlook.jp>
2019-03-25 01:46:02 +0000

    add column

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,7 @@
          @include('commons.navbar')
         
         <div class="container">
+            @include('commons.error_messages')
             @yield('content')
         </div>
         
